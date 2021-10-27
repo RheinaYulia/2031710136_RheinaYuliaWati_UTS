391f3114cbe01b1054b29ac09d3331819b73f657
RheinaYulia <rheinayulia27@gmail.com>
2021-10-27 22:26:29 +0700

    Menambahkan commit file

---
+++ resources/views/home.blade.php
@@ -0,0 +1,454 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Outing : Home</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/icon" href="assets/images/favicon.ico"/>
+    <!-- Font Awesome -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+     <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <!-- Slick slider -->
+    <link href="assets/css/slick.css" rel="stylesheet">
+    <!-- Theme color -->
+    <link id="switcher" href="assets/css/theme-color/default-theme.css" rel="stylesheet">
+
+    <!-- Main Style -->
+    <link href="style.css" rel="stylesheet">
+
+    <!-- Fonts -->
+
+    <!-- Poppins For Title -->
+	<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+ 
+ 
+	</head>
+  <body>
+
+  
+
+   <!--START SCROLL TOP BUTTON -->
+    <a class="scrollToTop" href="#">
+      <i class="fa fa-angle-up"></i>
+    </a>
+  <!-- END SCROLL TOP BUTTON -->
+  	
+  	<!-- Start Header -->
+	<header id="mu-hero">
+		<div class="container">
+			<div class="mu-hero-area">
+				
+				<div class="mu-hero-top">
+					<!-- Start center Logo -->
+					<div class="mu-logo-area">
+						<!-- text based logo -->
+						<a class="mu-logo" href="home"><span>Home</span></a>
+						<!-- Image based logo -->
+						<!-- <a class="mu-logo" href="index.html"><img src="assets/images/logo.jpg" alt="logo img"></a> -->
+					</div>
+					<!-- End center Logo -->
+					<div class="mu-hero-top-info">
+						<ul>
+							<li>
+								<i class="fa fa-envelope" aria-hidden="true"></i>
+								<span>rheinayulia27@gmail.com</span>
+							</li>
+							<li>
+								<div class="mu-telephone">
+									<i class="fa fa-phone" aria-hidden="true"></i>
+									<span>081803617265</span>
+								</div>
+							</li>
+						</ul>
+						
+					</div>
+				</div>
+
+				<!-- Start hero featured area -->
+				<div class="mu-hero-featured-area">
+					<div class="mu-hero-featured-content">
+						<h2>Selamat Datang !!! </h2>
+						<h1>Tempat Wisata di Batu Malang yang Paling Hits Dikunjungi</h1>
+						
+                        <p> Untuk menelusuri apa saja yang ada di Batu, beberapa tempat wisata berikut dapat dijadikan acuan. Selain itu, Anda mungkin menemukan tempat yang belum dikenal banyak orang. </p>
+						
+						<div class="mu-scrolldown-area">
+							<a href="#mu-about" class="mu-scrolldown" id="mu-scrolldown"><i class="fa fa-chevron-down" aria-hidden="true"></i></a>
+						</div>
+					</div>
+				</div>
+				<!-- End hero featured area -->
+
+			</div>
+		</div>
+	</header>
+	<!-- End Header -->
+
+	
+	<!-- Start main content -->
+	<main>
+		<!-- Start About -->
+		<section id="mu-about">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="mu-about-area">
+							<!-- Start Feature Content -->
+							<div class="row">
+								<div class="col-md-6">
+									<div class="mu-about-left">
+										<img class="" src="assets/images/alun.jpg" alt="img">
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="mu-about-right">
+										<h2>About Batu</h2>
+										<p>Batu menjadi destinasi wisata dengan berbagai tujuan menarik. 
+                            Anda bisa mengunjungi rekomendasi tempat wisata di Batu Malang yang populer seperti wisata sejarah, alam, modern, keluarga, kuliner, dan lainnya.
+                            Nama Batu dan Malang sudah sering didengar sebagai tujuan wisata favorit. Lokasinya dekat dengan gunung membuat daerah tersebut sangat sejuk dan segar.</p>
+									</div>
+								</div>
+							</div>
+							<!-- End Feature Content -->
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End About -->
+		
+		
+
+		<!-- Start Video -->
+		<section id="mu-video">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="mu-video-area">
+							<h2>10 Tempat Wisata Batu Terbaru</h2>
+							<p class="mu-title-content"> Jika anda ingin melihat bagaimana menarik dan serunya tempat wisata yang ada di Kota Batu, simak video sampai habis yaa !! </p>
+
+							<!-- Start Video content -->
+							<div class="mu-video-content">		
+								<iframe width="854" height="480" src="https://www.youtube.com/embed/Ei-s3JBMSFc" allowfullscreen></iframe>							
+							</div>
+							<!-- End Video content -->
+
+						</div>
+					</div>
+				</div>
+			</div>	
+		</section>
+		<!-- End Video -->
+
+		<!-- Start Featured Tours -->
+		<section id="mu-featured-tours">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="mu-featured-tours-area">
+							<h2>Wisata Pilihan di Kota Batu</h2>
+							<p class="mu-title-content">Berikut adalah wisata Pilihan yang terkenal dan terbaru yang tepatnya berada di kawasan wisata Kota Batu</p>
+
+							<!-- Start Featured Tours content -->
+							<div class="mu-featured-tours-content">
+								<div class="row">
+
+									<div class="col-md-4">
+										<div class="mu-featured-tours-single">
+											<img src="assets/images/jatim3.jpg" alt="img">
+											<div class="mu-featured-tours-single-info">
+												<h3>JATIM PARK 3 </h3>
+												<h4> Harga Tiket: Rp 75.000</h4>
+												
+												<p>Alamat: Jl. Ir. Soekarno No.144, Beji, Kec. Junrejo, Kota Batu, Jawa Timur.</p>
+												<a href="maps" class="mu-book-now-btn">Map</a>
+											</div>
+										</div>
+									</div>
+
+									<div class="col-md-4">
+										<div class="mu-featured-tours-single">
+											<img src="assets/images/taman.jpg" alt="img">
+											<div class="mu-featured-tours-single-info">
+												<h3>Taman Langit</h3>
+												<h4> Harga Tiket: Rp 10.000</h4>
+												
+												<p>Alamat: Jl. Gn. Banyak, Gunungsari, Kec. Bumiaji, Kota Batu, Jawa Timur.</p>
+												<a href="maps1" class="mu-book-now-btn">Map</a>
+											</div>
+										</div>
+									</div>
+
+									<div class="col-md-4">
+										<div class="mu-featured-tours-single">
+											<img src="assets/images/selecta.jpg" alt="img">
+											<div class="mu-featured-tours-single-info">
+												<h3>Selecta</h3>
+												<h4> Harga Tiket: Rp 40.000 </h4>
+												<p>Alamat: Jl. Raya Selecta No.1, Tulungrejo, Kec. Bumiaji, Kota Batu, Jawa Timur.</p>
+												<a href="maps2" class="mu-book-now-btn">Map</a>
+											</div>
+										</div>
+									</div>
+										<div class="col-md-4">
+										<div class="mu-featured-tours-single">
+											<img src="assets/images/paralayang.jpg" alt="img">
+											<div class="mu-featured-tours-single-info">
+												<h3>Paralayang</h3>
+												<h4> Harga Tiket: Rp 10.000 </h4>
+												
+												<p>Alamat: Jl. Arumdalu No.20, Songgokerto, Kec. Batu, Kota Batu, Jawa Timur.</p>
+												<a href="maps3" class="mu-book-now-btn">Map</a>
+											</div>
+										</div>
+									</div>
+
+									<div class="col-md-4">
+										<div class="mu-featured-tours-single">
+											<img src="assets/images/flora.jpg" alt="img">
+											<div class="mu-featured-tours-single-info">
+												<h3>Flora Wisata San Terra</h3>
+												<h4>Harga Tiket: Rp 20.000 </h4>
+												
+												<p>Alamat: Jl. Truno Joyo, Jurangrejo, Pandesari, Kec. Pujon, Kab. Malang, Jawa Timur.</p>
+												<a href="maps4" class="mu-book-now-btn">Map</a>
+											</div>
+										</div>
+									</div>
+
+									<div class="col-md-4">
+										<div class="mu-featured-tours-single">
+											<img src="assets/images/bns.jpeg" alt="img">
+											<div class="mu-featured-tours-single-info">
+												<h3>Batu Night Spectacular</h3>
+												<h4> Harga Tiket: Rp 35.000</h4>
+												<p>Alamat: Jl. Hayam Wuruk No.1, Oro-Oro Ombo, Kec. Batu, Kota Batu, Jawa Timur.</p>
+												<a href="maps5" class="mu-book-now-btn">Map</a>
+											</div>
+										</div>
+									</div>
+
+								</div>
+							</div>
+							<!-- End Featured Tours content -->
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Featured Tours -->
+
+		<!-- Start Client Testimonials -->
+		<section id="mu-testimonials">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="mu-testimonials-area">
+							<h2>Tentang Tempat Wisata</h2>
+
+							<div class="mu-testimonials-block">
+								<ul class="mu-testimonial-slide">
+
+									<li>
+										<i class="fa fa-quote-left mu-client-quote" aria-hidden="true"></i>
+										<p>"Wisata ini mengedepankan pemandangan alami dan spot unik untuk mengambil gambar. Anda area dengan background angsa untuk mereka yang ingin mendapatkan sensasi terbang. "</p>
+										<img class="mu-rt-img" src="assets/images/taman.jpg" alt="img">
+										<h5 class="mu-rt-name"> Taman Langit</h5>
+										<span class="mu-rt-title">Bumiaji,Batu </span>
+									</li>
+
+									<li>
+										<i class="fa fa-quote-left mu-client-quote" aria-hidden="true"></i>
+										<p>"Tempat wisata satu ini memberikan sensasi seperti kembali ke jaman prasejarah dan bertemu dengan sekawanan makluk purbakala. Disebut Dino Park karena disini Anda akan bertemu langsung dengan kawanan dinosaurus ala film Jurassic Park."</p>
+										<img class="mu-rt-img" src="assets/images/testimonials-bg.jpeg" alt="img">
+										<h5 class="mu-rt-name"> Jatim Park 3</h5>
+										<span class="mu-rt-title">Junrejo,Batu</span>
+									</li>
+
+									<li>
+										<i class="fa fa-quote-left mu-client-quote" aria-hidden="true"></i>
+										<p>"Taman wisata ini bertemakan aneka bunga dengan keindahan alamnya. Suasana yang disuguhkan begitu sejuk dan segar karena berada di ketinggian 1100 mdpl. "</p>
+										<img class="mu-rt-img" src="assets/images/selecta.jpg" alt="img">
+										<h5 class="mu-rt-name"> Selecta</h5>
+										<span class="mu-rt-title">Bumiaji,Batu</span>
+									</li>
+
+								</ul>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Client Testimonials -->
+
+		<!-- Start Clients -->
+		<section id="mu-clients">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="mu-clients-area">
+							<h2>Awesome Place</h2>
+
+							<!-- Start Clients brand logo -->
+							<div class="mu-clients-slider">
+
+								<div class="mu-clients-single">
+									<img src="assets/images/paralayang.jpg" alt="Brand Logo">
+								</div>
+
+								<div class="mu-clients-single">
+									<img src="assets/images/jatim3.jpg" alt="Brand Logo">
+								</div>
+
+								<div class="mu-clients-single">
+									<img src="assets/images/selecta.jpg" alt="Brand Logo">
+								</div>
+
+								<div class="mu-clients-single">
+									<img src="assets/images/testimonials-bg.jpeg" alt="Brand Logo">
+								</div>
+
+								<div class="mu-clients-single">
+									<img src="assets/images/taman.jpg" alt="Brand Logo">
+								</div>
+
+								<div class="mu-clients-single">
+									<img src="assets/images/about-video-bg.jpeg" alt="Brand Logo">
+								</div>
+							</div>
+							<!-- End Clients brand logo -->
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Clients -->
+
+		<!-- Start Call to Action -->
+		<section id="mu-callto-action">
+			<div class="container">
+				<div class="row col-md-12">
+					<div class="mu-callto-action-area">
+						<h2>Thank you for visiting</h2>
+						<a class="mu-book-now-btn" href="#">Start Journey</a>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Start Call to Action -->
+
+		<!-- Start Contact -->
+		<section id="mu-contact">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="mu-contact-area">
+							<h2>Contact Us</h2>
+							
+							<!-- Start Contact Content -->
+							<div class="mu-contact-content">
+								<div class="row">
+
+									<div class="col-md-12">
+										<div class="mu-contact-form-area">
+											<div id="form-messages"></div>
+											<form id="ajax-contact" method="post" action="mailer.php" class="mu-contact-form">
+												<div class="row">
+													<div class="col-md-6">
+														<div class="form-group">                
+															<input type="text" class="form-control" placeholder="Name" id="name" name="name" required>
+														</div>
+													</div>
+
+													<div class="col-md-6">
+														<div class="form-group">                
+															<input type="email" class="form-control" placeholder="Enter Email" id="email" name="email" required>
+														</div>    
+													</div>          
+												</div>
+
+												<div class="form-group">                
+													<input type="text" class="form-control" placeholder="Your Subject" id="subject" name="subject" required>
+												</div>
+
+												<div class="form-group">
+													<textarea class="form-control" placeholder="Message" id="message" name="message" required></textarea>
+												</div>
+												<button type="submit" class="mu-send-msg-btn"><span>Send Message</span></button>
+								        	</form>
+										</div>
+									</div>
+
+								</div>
+							</div>
+							<!-- End Contact Content -->
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Contact -->
+
+
+	</main>
+	
+	<!-- End main content -->	
+			
+			
+	<!-- Start footer -->
+	<footer id="mu-footer">
+		<div class="container">
+			<div class="mu-footer-area">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="mu-footer-left">
+							<p class="mu-copy-right">&copy; Copyright <a rel="nofollow" href="http://markups.io">markups.io</a>. All right reserved.</p>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="mu-footer-right">
+							<div class="mu-social-media">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+								<a href="#"><i class="fa fa-google-plus"></i></a>
+								<a href="#"><i class="fa fa-linkedin"></i></a>
+								<a href="#"><i class="fa fa-youtube"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</footer>
+	<!-- End footer -->
+
+	
+	<!-- JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+	<!-- Slick slider -->
+    <script type="text/javascript" src="assets/js/slick.min.js"></script>
+    <!-- Ajax contact form  -->
+    <script type="text/javascript" src="assets/js/app.js"></script>
+  
+      
+	
+    <!-- Custom js -->
+	<script type="text/javascript" src="assets/js/custom.js"></script>
+
+	
+	
+    
+  </body>
+</html>
---
+++ resources/views/maps3.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MAP</title>
+
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+   integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+   crossorigin=""/>
+
+    <!-- Make sure you put this AFTER Leaflet's CSS -->
+ <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+   integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+   crossorigin=""></script>
+
+    <style> 
+    #mapid { 
+        margin-top: 30px;
+        height: 600px; } 
+    #link a
+    {
+        background-color: green;
+        color : white;
+        padding-top : 20px;
+        padding-bottom : 20px;
+        text-decoration:none;
+        padding-right:10px;
+        padding-left:10px;
+        border: 4px solid black;
+        border-radius: 10px;
+    
+    }
+    </style>
+
+</head>
+<body>
+    <div id="link">
+    <h1> <a href='/'> Home </a> </h1>
+    </div>
+    <div id="mapid"></div>
+
+</body>
+
+<script> 
+    var mymap = L.map('mapid').setView([-7.8550235,112.4945427], 17); 
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox/streets-v11',
+		tileSize: 512,
+		zoomOffset: -1
+	}).addTo(mymap);
+
+    L.marker([-7.8550235,112.4945427]).addTo(mymap);
+</script>
+</html>
